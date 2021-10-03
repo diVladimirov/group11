7eae5739b54f0c2f7bc577fe9917f9ff43f151e4
Andrii Miroshnichenko <andy.miroshnichenko@gmail.com>
2021-10-03 12:32:57 +0300

    vehicles

--- js/index.js
+++ js/index.js
@@ -244,3 +244,32 @@
 // console.log(TopLevelWorker.__proto__ === Worker.__proto__);
 // console.log(mangoWorker.__proto__ === TopLevelWorker.prototype);
 // console.log(TopLevelWorker.prototype.__proto__ === Worker.prototype);
+
+
+//1.Реализовать фильтер по свойству amount и получить
+//только название модели
+
+
+
+// const vehicles = [
+//   { make: 'Honda', model: 'CR-V', type: 'suv', amount: 14, price: 24045, onSale: true },
+//   { make: 'Honda', model: 'Accord', type: 'sedan', amount: 2, price: 22455, onSale: true },
+//   { make: 'Mazda', model: 'Mazda 6', type: 'sedan', amount: 8, price: 24195, onSale: false },
+//   { make: 'Mazda', model: 'CX-9', type: 'suv', amount: 7, price: 31520, onSale: true },
+//   { make: 'Toyota', model: '4Runner', type: 'suv', amount: 19, price: 34210, onSale: false },
+//   { make: 'Toyota', model: 'Sequoia', type: 'suv', amount: 16, price: 45560, onSale: false },
+//   { make: 'Toyota', model: 'Tacoma', type: 'truck', amount: 4, price: 24320, onSale: true },
+//   { make: 'Ford', model: 'F-150', type: 'truck', amount: 11, price: 27110, onSale: true },
+//   { make: 'Ford', model: 'Fusion', type: 'sedan', amount: 13, price: 22120, onSale: true },
+//   { make: 'Ford', model: 'Explorer', type: 'suv', amount: 6, price: 31660, onSale: false },
+// ];
+
+// const getAvailableCarNames = (cars, value) => {
+//     return cars.filter(({amount}) => amount > value).map(({model}) => model);
+// }
+// console.table(getAvailableCarNames(vehicles, 12));
+// //2.Получить машины на распродаже и сортировать по убыванию цены
+// const getSortedCarsOnSale = (cars) => {
+//     return cars.filter(({onSale}) => onSale).sort((prevCar, nextCar) => nextCar.price - prevCar.price)
+// }
+// console.table(getSortedCarsOnSale(vehicles));
