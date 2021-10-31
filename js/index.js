1e87e1ecaa4e2dd9a6a1511b6cad1c3b629d0cea
Vadim Vladimirov <kurtonchik@gmail.com>
2021-10-31 13:03:32 +0200

    add hometask

--- js/index.js
+++ js/index.js
@@ -448,3 +448,12 @@
 //   }
 //   passInfoEl.style.display = "none";
 // }
+
+const a = [121, 144, 19, 161, 19, 144, 19, 11];
+const b = [121, 14641, 20736, 361, 25921, 361, 20736, 361];
+
+console.log(comp(a, b));
+
+function comp() {}
+
+//
